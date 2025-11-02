e0bc3f83c78bf8834707d97e3c46ee44568016f2
thanghv <thang@mainwp.com>
2025-11-03 00:30:51 +0700

    Update: version number 6.0-beta.8

--- class/class-mainwp-system.php
+++ class/class-mainwp-system.php
@@ -29,7 +29,7 @@ class MainWP_System { // phpcs:ignore Generic.Classes.OpeningBraceSameLine.Conte
      *
      * @var string Current plugin version.
      */
-    public static $version = '5.4.0.23'; // NOSONAR.
+    public static $version = '6.0-beta.8'; // NOSONAR.
 
     /**
      * Private static variable to hold the single instance of the class.
