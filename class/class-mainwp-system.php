f96d619b5fa5b0a39823737c8c20c3f4916f563d
thanghv <thang@mainwp.com>
2025-11-03 13:29:21 +0700

    Update: beta 11

--- class/class-mainwp-system.php
+++ class/class-mainwp-system.php
@@ -29,7 +29,7 @@ class MainWP_System { // phpcs:ignore Generic.Classes.OpeningBraceSameLine.Conte
      *
      * @var string Current plugin version.
      */
-    public static $version = '6.0-beta.8'; // NOSONAR.
+    public static $version = '6.0-beta.11'; // NOSONAR.
 
     /**
      * Private static variable to hold the single instance of the class.
--- mainwp.php
+++ mainwp.php
@@ -8,7 +8,7 @@
  * Author URI: https://mainwp.com
  * Plugin URI: https://mainwp.com/
  * Text Domain: mainwp
- * Version:  6.0-beta.8
+ * Version:  6.0-beta.11
  *
  * @package MainWP/Dashboard
  *
